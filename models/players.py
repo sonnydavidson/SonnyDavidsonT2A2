55ee50cbbec250bdbe19cb6f88f6cbf7cbbb065a
sonnydavidson <110369771+sonnydavidson@users.noreply.github.com>
2022-11-13 00:33:15 +1000

    Final commit

--- models/players.py
+++ models/players.py
@@ -4,14 +4,16 @@ class Player(db.Model):
     __tablename__ = 'players'
 
     position = db.Column(db.String, nullable=False)
-    team = db.Column(db.String(50), nullable=False)
-    name = db.Column(db.String(50), primary_key=True)
+    team = db.Column(db.String(50), primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
     number = db.Column(db.Integer, nullable=False)
     goals = db.Column(db.Integer, nullable=False)
     assists = db.Column(db.Integer, nullable=False)
     cleansheets= db.Column(db.Integer, nullable=False)
     form = db.Column(db.String, nullable=False)
     fitness = db.Column(db.String, nullable=False)
+
+    player_team = db.relationship('Table', back_populates='playersteam', cascade='all, delete')
 
 class PlayerSchema(ma.Schema):
     class Meta:
